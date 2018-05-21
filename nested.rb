bae350d6c9d05826d166604e165c69d4d8aa3a2b
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 18:40:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,7 @@ def adding_matz
         }
      }
 
-		 programmer_hash.first = :yukihiro_matsumoto
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP, C"]}
 end
 
 def changing_alan
